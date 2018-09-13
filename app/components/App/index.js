66736d13685d0a6739282e663dec7cbb3f84d53f
pengfluf <pengfluf@tutanota.com>
2018-09-13 20:45:12 +0300

    Initial Boilerplate

---
+++ app/components/App/index.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * App
+ *
+ * This component is the skeleton around the actual pages, and should only
+ * contain code that should be seen on all pages. (e.g. navigation bar)
+ */
+
+import React from 'react';
+
+function App() {
+  return <div>Hello</div>;
+}
+
+export default App;
